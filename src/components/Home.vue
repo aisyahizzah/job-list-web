5ec933e022fd5166c3d0d240336cea0d86d8330d
aisyah_izzah <13517046@std.stei.itb.ac.id>
2020-03-23 15:09:21 +0700

    Add job tags

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,14 +8,17 @@
               <div>
                 <img class="company_logo" src="../assets/logo.png">
               </div>
-              <div>
+              <div class="job_info">
                 <div class="job_position"> {{ job.position }} </div>
                 <div class="company_name"> {{ job.name }} </div>
                 <div class="job_location"> {{ job.location }} </div>
-                <div class="job_info"> {{ job.type }} · {{ job.salary }} </div>
+                <div class="job_detail"> {{ job.type }} · {{ job.salary }} </div>
               </div>
               <div>
                 <div class=job_timestamp>Posted {{ job.posted }} days ago</div>
+                <div class="tags_container">
+                  <div class="job_tag" v-bind:key="tag" v-for="tag in job.tag">{{tag}}</div>
+                </div>
               </div>
             </div>
           </el-card>
@@ -42,10 +45,10 @@ export default {
   data: function() {
     return { 
       jobs: [
-        { position: 'Software Engineer', name: 'MyCompany', location: "Jakarta, Indonesia", type: "Full-time", salary: "2,500,000 IDR/month", posted:2},
-        { position: 'Front-end Engineer', name: 'HelloWorld', location: "Bandung, Indonesia", type: "Part-time", salary: "3,500,000 - 4,000,000 IDR/month", posted:3},
-        { position: 'Data Analyst', name: 'Strawberry', location: "Bandung, Indonesia", type: "Full-time", salary: "2,500,000 - 3,500,000 IDR/month", posted:5},
-        { position: 'AI Engineer', name: 'Panda', location: "Jakarta, Indonesia", type: "Part-time", salary: "3,500,000 - 4,500,000 IDR/month", posted:10},
+        { position: 'Software Engineer', name: 'MyCompany', location: "Jakarta, Indonesia", type: "Full-time", salary: "2,500,000 IDR/month", posted:2, tag: ["Intern","Fullstack","Javascript","PHP"]},
+        { position: 'Front-end Engineer', name: 'HelloWorld', location: "Bandung, Indonesia", type: "Part-time", salary: "3,500,000 - 4,000,000 IDR/month", posted:3, tag: ["Intern","Frontend","Java"]},
+        { position: 'Data Analyst', name: 'Strawberry', location: "Bandung, Indonesia", type: "Full-time", salary: "2,500,000 - 3,500,000 IDR/month", posted:5, tag: ["Junior","Data","NoSQL"]},
+        { position: 'AI Engineer', name: 'Panda', location: "Jakarta, Indonesia", type: "Part-time", salary: "3,500,000 - 4,500,000 IDR/month", posted:10, tag: ["Senior","Python"]},
       ]
     }
   },
@@ -99,6 +102,11 @@ export default {
 
 .job_container{
   display: flex;
+  justify-content: space-between;
+}
+
+.job_info{
+  flex-basis: 800px;
 }
 
 .company_name{
@@ -122,9 +130,30 @@ export default {
   font-size: 13px;
 }
 
-.job_info{
+.job_detail{
   color: #9C9B9B;
   font-size: 13px;
+}
+
+.job_timestamp{
+  text-align: right;
+  color: #9C9B9B;
+  font-size: 13px;
+}
+
+.tags_container{
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5px;
+}
+
+.job_tag{
+  background-color: #F0FAFB;
+  padding: 5px 10px;
+  margin-left: 10px;
+  color: #5DA5A4;
+  font-size: 15px;
+  font-weight: 600;
 }
 
 </style>
